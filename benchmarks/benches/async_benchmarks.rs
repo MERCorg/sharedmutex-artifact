7d72bab4caa9c653d6545e1b81c8fb07bb62609f
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-08 17:31:55 +0100

    Added the benchmark setup using criterion

---
+++ benchmarks/benches/async_benchmarks.rs
@@ -0,0 +1,58 @@
+use std::{hint::black_box, sync::Arc};
+
+// This is a struct that tells Criterion.rs to use the "futures" crate's current-thread executor
+use criterion::Criterion;
+
+use rand::{prelude::*, distr::Bernoulli};
+use tokio::runtime::Runtime;
+
+use benchmarks::{THREADS, READ_RATIOS, NUM_ITERATIONS};
+
+pub fn benchmark_async(c: &mut Criterion) {
+    
+    for num_threads in THREADS {
+        for read_ratio in READ_RATIOS {
+            c.bench_function(&format!("tokio::sync::RwLock {} {} {}", num_threads, NUM_ITERATIONS, read_ratio), |bencher| {
+                
+                bencher.iter(|| {  
+                    let rt = Runtime::new().unwrap();
+                    let shared_vector = Arc::new(tokio::sync::RwLock::<Vec::<i32>>::new(vec![]));
+                    let dist = Bernoulli::from_ratio(1, read_ratio).unwrap();
+
+                    let mut tasks = vec![];
+                    for _ in 1..num_threads {
+                        let shared_vector = shared_vector.clone();
+                        tasks.push(rt.spawn(async move {
+                            for _ in 0..NUM_ITERATIONS {
+                                let random = {
+                                    let mut rng = rand::rng();
+                                    dist.sample(&mut rng)
+                                };
+
+                                if random {
+                                    // Read a random index.
+                                    let read = shared_vector.read().await;
+                                    if read.len() > 0 {
+                                        let mut rng = rand::rng();  
+                                        let index = rng.random_range(0..read.len());
+                                        black_box(assert_eq!(read[index], 5));
+                                    }
+                                } else {
+                                    // Add a new vector element.
+                                    shared_vector.write().await.push(5);
+                                }
+                            }
+                        }));
+                    };
+
+                    // Wait for tasks to complete.
+                    rt.block_on(async move { 
+                        for task in tasks {
+                            task.await.unwrap()
+                        }
+                    });
+                })
+            });
+        }
+    }
+}
---
+++ benchmarks/benches/mutex_benchmarks.rs
@@ -0,0 +1,188 @@
+use std::sync::{Arc, RwLock, Mutex};
+
+use criterion::Criterion;
+
+use merc_sharedmutex::BfSharedMutex;
+
+use benchmarks::{benchmark, NUM_ITERATIONS, READ_RATIOS, THREADS};
+
+/// Benchmark the bfsharedmutex implementation
+pub fn benchmark_bfsharedmutex(c: &mut Criterion) {
+    for num_threads in THREADS {
+        for read_ratio in READ_RATIOS {
+
+            // Benchmark various configurations.
+            benchmark(
+                c,
+                "bf-sharedmutex::BfSharedMutex",
+                BfSharedMutex::new(()),
+                |shared| {
+                    let _guard = shared.read().unwrap();
+                },
+                |shared| {
+                    let _guard = shared.write().unwrap();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+        }
+    }
+}
+
+// Split up to first do our own benchmarks since than we can update the implementation easily.   
+pub fn benchmark_othermutexes(c: &mut Criterion) {
+    for num_threads in THREADS {
+        benchmark(
+            c,
+            "std::sync::Mutex",
+            Arc::new(Mutex::new(())),
+            |_| {
+            },
+            |shared| {
+                let _guard = shared.lock();
+            },
+            num_threads,
+            NUM_ITERATIONS,
+            1,
+        );
+
+        for read_ratio in READ_RATIOS {
+
+            benchmark(
+                c,
+                "std::sync::RwLock",
+                Arc::new(RwLock::new(())),
+                |shared| {
+                    let _guard = shared.read().unwrap();
+                },
+                |shared| {
+                    let _guard = shared.write().unwrap();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            benchmark(
+                c,
+                "parking_lot::RwLock",
+                Arc::new(parking_lot::RwLock::new(())),
+                |shared| {
+                    let _guard = shared.read();
+                },
+                |shared| {
+                    let _guard = shared.write();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            benchmark(
+                c,
+                "spin::RwLock",
+                Arc::new(spin::RwLock::new(())),
+                |shared| {
+                    shared.read();
+                },
+                |shared| {
+                    shared.write();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            // This lock seems to deadlock.
+            // benchmark(
+            //     c,
+            //     "widerwlock::WideRwLock",
+            //     Arc::new(widerwlock::WideRwLock::new(())),
+            //     |shared| {
+            //         shared.read();
+            //     },
+            //     |shared| {
+            //         shared.write();
+            //     },
+            //     num_threads,
+            //     NUM_ITERATIONS,
+            //     read_ratio,
+            // );
+
+            benchmark(
+                c,
+                "shared_mutex::SharedMutex",
+                Arc::new(shared_mutex::SharedMutex::new(())),
+                |shared| {
+                    let _guard = shared.read();
+                },
+                |shared| {
+                    let _guard = shared.write();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            benchmark(
+                c,
+                "pairlock::RwLock",
+                Arc::new(pairlock::PairLock::with_default(())),
+                |shared| {
+                    shared.view(|_| {});
+                },
+                |shared| {
+                    shared.update();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            benchmark(
+                c,
+                "pflock::PFLock",
+                Arc::new(pflock::PFLock::new(())),
+                |shared| {
+                    let _guard = shared.read();
+                },
+                |shared| {
+                    let _guard = shared.write();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            benchmark(
+                c,
+                "crossbeam::ShardedLock",
+                Arc::new(crossbeam::sync::ShardedLock::new(())),
+                |shared| {
+                    let _guard = shared.read().unwrap();
+                },
+                |shared| {
+                    let _guard = shared.write().unwrap();
+                },
+                num_threads,
+                NUM_ITERATIONS,
+                read_ratio,
+            );
+
+            // This library might work on linux, but even there it does seem to have a weird API.
+            // #[cfg(target_os = "linux")]
+            // benchmark(c,
+            //     "process_sync::SharedMemoryObject",
+            //     Arc::new(process_sync::SharedMemoryObject::new(5).unwrap()),
+            //     |shared| {
+            //         shared.get();
+            //     }, |shared| {
+            //         shared.get_mut();
+            //     },
+            //     num_threads,
+            //     NUM_ITERATIONS,
+            //     read_ratio);
+        }
+    }
+}
